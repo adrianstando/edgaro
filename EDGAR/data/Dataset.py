Fixes, encoders in model, PDP for both categorical and numerical

EDGAR/data/Dataset.py
@@ -11,14 +11,18 @@ from typing import Optional
 
 class Dataset:
     def __init__(self, name: str, dataframe: Optional[pd.DataFrame], target: Optional[pd.Series]):
-        if target is not None:
-            unique = np.unique(target)
-            if len(unique) > 2:
-                raise Exception('This dataset is not correct for a binary classification task!')
-
         self.name = name
         self.data = dataframe
         self.target = target
+
+    def check_binary_classification(self):
+        if self.target is not None:
+            unique = np.unique(self.target)
+            if len(unique) > 2:
+                return False
+            return True
+        else:
+            return False
 
     def generate_report(self, output_path: Optional[str] = None, show_jupyter: bool = False):
         if self.data is None and self.target is None:
@@ -48,8 +52,6 @@ class Dataset:
             raise Exception('Target has too many classes for binary classification!')
         else:
             return max(counts) / min(counts)
-
-
 
 
 class DatasetFromCSV(Dataset):
@@ -88,7 +90,7 @@ class DatasetFromOpenML(Dataset):
             raise Exception('Provide needed arguments!')
 
         data = None
-        if openml_dataset is not None:
+        if openml_dataset is None:
             data = openml.datasets.get_dataset(task_id)
         else:
             data = openml_dataset
@@ -100,4 +102,4 @@ class DatasetFromOpenML(Dataset):
         X = pd.DataFrame(X, columns=attribute_names)
         y = pd.Series(y, name='target')
 
-        super(Dataset, self).__init__(name=data.name, dataframe=X, target=y)
+        super().__init__(name=data.name, dataframe=X, target=y)

EDGAR/model/Model.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
 from copy import deepcopy
+import numpy as np
 from typing import Optional
+
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
 from EDGAR.base.BaseTransformer import BaseTransformer
 from EDGAR.data.Dataset import Dataset
 
@@ -17,24 +22,53 @@ class Model(BaseTransformer, ABC):
         self.__transform_to_probabilities = False
         self.__train_dataset = None
         self.name = name
+        self.__label_encoders = {}
+        self.__target_encoder = None
 
     def fit(self, dataset: Dataset):
+        if not dataset.check_binary_classification():
+            raise Exception('Dataset does not represent binary classification task!')
+
         self.__train_dataset = deepcopy(dataset)
+        ds = deepcopy(dataset)
+
+        columns_to_encode = list(dataset.data.select_dtypes(include=['category', 'object']))
+        for col in columns_to_encode:
+            le = LabelEncoder()
+            ds.data[col] = le.fit_transform(ds.data[col])
+            self.__label_encoders[col] = le
+
+        self.__target_encoder = _TargetEncode()
+        ds.target = self.__target_encoder.fit_transform(ds.target)
+        self.__train_dataset.target = self.__target_encoder.transform(self.__train_dataset.target)
+
         if self.name == '':
             self.name = dataset.name
 
-        return self.__fit(dataset)
+        return self._fit(ds)
 
     @abstractmethod
-    def __fit(self, dataset: Dataset):
+    def _fit(self, dataset: Dataset):
         pass
 
-    @abstractmethod
     def predict(self, dataset: Dataset):
-        pass
+        df = deepcopy(dataset)
+        for key, le in self.__label_encoders.items():
+            df.data[key] = le.transform(df.data[key])
+        return self._predict(df)
 
     @abstractmethod
+    def _predict(self, dataset: Dataset):
+        pass
+
     def predict_proba(self, dataset: Dataset):
+        df = deepcopy(dataset)
+        for key, le in self.__label_encoders.items():
+            df.data[key] = le.transform(df.data[key])
+        return self._predict_proba(df)
+
+    @abstractmethod
+    def _predict_proba(self, dataset: Dataset):
         pass
 
     @abstractmethod
@@ -63,26 +97,61 @@ class Model(BaseTransformer, ABC):
     def get_train_dataset(self):
         return self.__train_dataset
 
+    def get_category_colnames(self):
+        return list(self.__label_encoders.keys())
+
+
+class _TargetEncode(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.mapping = None
+        super().__init__()
+
+    def fit(self, y):
+        names, counts = np.unique(y, return_counts=True)
+        if counts[0] < counts[1]:
+            index_min = 0
+            index_max = 1
+        else:
+            index_min = 1
+            index_max = 0
+
+        self.mapping = {
+            names[index_min]: 1,
+            names[index_max]: 0
+        }
+        return self
+
+    def transform(self, y):
+        return y.map(self.mapping)
+
 
 class RandomForest(Model):
     def __init__(self, name: Optional[str] = '', *args, **kwargs):
         super().__init__(name=name)
         self.__model = RandomForestClassifier(*args, **kwargs)
 
-    def __fit(self, dataset: Dataset):
+    def _fit(self, dataset: Dataset):
         if dataset.target is None:
             raise Exception('Target data is not provided!')
 
-        self.__model.fit(dataset.data, dataset.target)
+        return self.__model.fit(dataset.data, dataset.target)
 
-    def predict(self, dataset: Dataset):
+    def _predict(self, dataset: Dataset):
+        if isinstance(dataset.data, np.ndarray) and isinstance(self.get_train_dataset().data, pd.DataFrame):
+            dataset = deepcopy(dataset)
+            dataset.data = pd.DataFrame(dataset, columns=self.get_train_dataset().data.columns)
+
         return Dataset(
             name=dataset.name + '_predicted',
             dataframe=None,
             target=self.__model.predict(dataset.data)
         )
 
-    def predict_proba(self, dataset: Dataset):
+    def _predict_proba(self, dataset: Dataset):
+        if isinstance(dataset.data, np.ndarray) and isinstance(self.get_train_dataset().data, pd.DataFrame):
+            dataset = deepcopy(dataset)
+            dataset.data = pd.DataFrame(dataset, columns=self.get_train_dataset().data.columns)
+
         return Dataset(
             name=dataset.name + '_predicted_probabilities',
             dataframe=None,