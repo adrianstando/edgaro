8e413f41f65066c692d564d9acfa114ed288508a
adrianstando <adrsnek11@live.com>
2022-10-08 23:59:06 +0200

    Add more model tests

--- test/test_model.py
+++ test/test_model.py
@@ -1,6 +1,6 @@
 import pytest
 from sklearn.ensemble import RandomForestClassifier
-from EDGAR.data.Dataset import DatasetFromOpenML
+from EDGAR.data.Dataset import Dataset, DatasetFromOpenML
 from EDGAR.model.Model import RandomForest, ModelFromSKLEARN
 from .resources.objects import *
 
@@ -33,5 +33,24 @@ def test_model(ds):
         model.fit(ds)
         model.predict(ds)
         model.predict_proba(ds)
+    except (Exception,):
+        assert False
+
+
+@pytest.mark.parametrize('ds', [
+    DatasetFromOpenML(task_id=task_id_1),
+    DatasetFromOpenML(task_id=task_id_2),
+])
+def test_model(ds):
+    try:
+        ds.remove_nans()
+
+        model = RandomForest()
+        model.fit(ds)
+        y = model.predict(ds)
+        assert isinstance(y, Dataset)
+        assert y.check_binary_classification()
+        y = model.predict_proba(ds)
+        assert isinstance(y, Dataset)
     except (Exception,):
         assert False
