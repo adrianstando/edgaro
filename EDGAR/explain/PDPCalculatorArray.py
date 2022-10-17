5fe75bf510efc9434812ac7eb8bb5b01ec39bd1c
adrianstando <adrsnek11@live.com>
2022-10-17 19:35:43 +0200

    Make PDPCalculatorArray iterable and add update implementation for higher dimensions

--- EDGAR/explain/PDPCalculatorArray.py
+++ EDGAR/explain/PDPCalculatorArray.py
@@ -1,30 +1,51 @@
-from typing import Union
+from typing import Union, Optional
 from EDGAR.model.Model import Model
 from EDGAR.model.ModelArray import ModelArray
 from EDGAR.explain.PDPCalculator import PDPCalculator
 
 
-# TODO:
-# deal with variables argument
-
 class PDPCalculatorArray:
-    def __init__(self, models: Union[Model, ModelArray]):
+    def __init__(self, models: Union[Model, ModelArray], N: Optional[int] = None):
         if isinstance(models, Model):
             models = ModelArray
         self.models = models
         self.sub_calculators = None
         self.name = models.name
+        self.N = N
 
     def fit(self):
-        def create_sub_calculator(model: Model):
-            calc = PDPCalculator(model)
+        def create_sub_calculator(model: Union[Model, ModelArray]):
+            if isinstance(model, Model):
+                calc = PDPCalculator(model=model, N=self.N)
+            else:
+                calc = PDPCalculatorArray(models=model, N=self.N)
+
             calc.fit()
             return calc
 
         self.sub_calculators = [create_sub_calculator(m) for m in self.models.get_models()]
 
-    def transform(self, variables=None, N: int = 1000):
+    def transform(self, variables=None):
         if variables is None:
-            return [calc.transform(N=N) for calc in self.sub_calculators]
+            return [calc.transform() for calc in self.sub_calculators]
         else:
-            return self.transform(variables=None)
+            def transform_given_variables(calc):
+                if isinstance(calc, PDPCalculator):
+                    var = list(set(variables).intersection(calc.model.get_train_dataset().data.columns))
+                    return calc.transform(variables=var)
+                else:
+                    return calc.transform(variables=variables)
+
+            return [transform_given_variables(calc) for calc in self.sub_calculators]
+
+    def __iter__(self):
+        self.current_i = 0
+        return self
+
+    def __next__(self):
+        if self.current_i < len(self.sub_calculators):
+            out = self.sub_calculators[self.current_i]
+            self.current_i += 1
+            return out
+        else:
+            raise StopIteration
--- test/test_explain_and_flow.py
+++ test/test_explain_and_flow.py
@@ -1,6 +1,6 @@
 import pytest
 from EDGAR.data.Dataset import DatasetFromOpenML, Dataset
-from EDGAR.balancing.Transformer import TransformerFromIMBLEARN
+from EDGAR.balancing.Transformer import TransformerFromIMBLEARN, RandomUnderSampler as RUS
 from EDGAR.balancing.TransformerArray import TransformerArray
 from imblearn.under_sampling import RandomUnderSampler
 from EDGAR.model.Model import RandomForest
@@ -26,12 +26,15 @@ def test_flow(df):
         df_new = transformator.transform(df)
         df_new.imbalance_ratio()
 
-        rf = RandomForest()
+        rf = RandomForest(test_size=0.1)
         rf.fit(df_new)
-        y = rf.predict(df_new)
-        accuracy_score(y.target, rf.get_train_dataset().target)
+        rf.evaluate()
 
-        pdp = PDPCalculator(rf)
+        test = rf.get_test_dataset()
+        y = rf.predict(test)
+        accuracy_score(y.target, rf.transform_target(test).target)
+
+        pdp = PDPCalculator(rf, N=200)
         pdp.fit()
 
         col = df.data.columns[0]
@@ -48,8 +51,9 @@ def test_flow(df):
 
 
 @pytest.mark.parametrize('df', [
-    DatasetArray([DatasetFromOpenML(task_id=task_id_1, apikey=APIKEY), DatasetFromOpenML(task_id=task_id_2, apikey=APIKEY)]),
-    DatasetArray([Dataset(name_1, df_1, target_1), Dataset(name_2, df_2, target_2)])
+    DatasetArray(
+        [DatasetFromOpenML(task_id=task_id_1, apikey=APIKEY), DatasetFromOpenML(task_id=task_id_2, apikey=APIKEY)]),
+    DatasetArray([Dataset(name_1, df_3, target_3), Dataset(name_2, df_3, target_3)])
 ])
 def test_flow_array(df):
     try:
@@ -57,20 +61,27 @@ def test_flow_array(df):
         df[0].imbalance_ratio()
         df[1].imbalance_ratio()
 
-        transformator = TransformerArray(TransformerFromIMBLEARN(RandomUnderSampler(sampling_strategy=1, random_state=42)))
+        transformator = TransformerArray(
+            TransformerFromIMBLEARN(RandomUnderSampler(sampling_strategy=1, random_state=42)))
         transformator.fit(df)
         df_new = transformator.transform(df)
 
         df_new[0].imbalance_ratio()
         df_new[1].imbalance_ratio()
 
-        rf = ModelArray(RandomForest())
+        rf = ModelArray(RandomForest(test_size=0.1))
         rf.fit(df_new)
-        y = rf.predict(df_new)
 
-        accuracy_score(y[0].target, rf.get_models()[0].get_train_dataset().target)
+        for m in rf:
+            m.evaluate()
 
-        pdp = PDPCalculatorArray(rf)
+        for i in range(len(rf)):
+            m = rf.get_models()[i]
+            test = m.get_test_dataset()
+            y = m.predict(test)
+            accuracy_score(y.target, m.transform_target(test).target)
+
+        pdp = PDPCalculatorArray(rf, N=200)
         pdp.fit()
         t = pdp.transform()
         t[0].plot(variable=df[0].data.columns[0])
@@ -84,6 +95,85 @@ def test_flow_array(df):
         t[0].compare(t1[0])
         t[0].compare(t1[0], variable=df[0].data.columns[0])
         t[0].compare(t1[0], variable=[df[0].data.columns[0], df[0].data.columns[1]])
+
+        t_2 = pdp.transform(variables=[df[0].data.columns[0], df[0].data.columns[1]])
+        t_2[0].plot(variable=df[0].data.columns[0])
+        t_2[0].plot(variable=df[0].data.columns[1])
+
+        try:
+            t_2[0].plot(variable=df[0].data.columns[2])
+            assert False
+        except (Exception,):
+            pass
+
+    except (Exception,):
+        assert False
+
+
+@pytest.mark.parametrize('df', [
+    DatasetArray([
+        DatasetFromOpenML(task_id=task_id_1, apikey=APIKEY),
+        DatasetFromOpenML(task_id=task_id_2, apikey=APIKEY),
+        DatasetArray([Dataset(name_1, df_3, target_3), Dataset(name_2, df_3, target_3)])
+    ])
+])
+def test_flow_array_of_arrays(df):
+    try:
+        df.remove_nans()
+        df[0].imbalance_ratio()
+        df[1].imbalance_ratio()
+        df[2][0].imbalance_ratio()
+        df[2][1].imbalance_ratio()
+
+        transformator = TransformerArray(RUS(imbalance_ratio=1, random_state=42))
+        transformator.fit(df)
+        df_new = transformator.transform(df)
+
+        df_new[0].imbalance_ratio()
+        df_new[1].imbalance_ratio()
+        df_new[2][0].imbalance_ratio()
+        df_new[2][1].imbalance_ratio()
+
+        rf = ModelArray(RandomForest(test_size=0.1))
+        rf.fit(df_new)
+
+        def evaluate(model):
+            if isinstance(model, ModelArray):
+                for mod in model:
+                    evaluate(mod)
+            else:
+                model.evaluate()
+
+        evaluate(rf)
+
+        pdp = PDPCalculatorArray(rf, N=200)
+        pdp.fit()
+        t = pdp.transform()
+        t[0].plot(variable=df[0].data.columns[0])
+
+        t1 = deepcopy(t)
+        t1[0].results[df[0].data.columns[0]].y += 0.1
+        t1[0].name += '_xx'
+
+        t[0].plot(variable=df[0].data.columns[0], add_plot=[t1[0]])
+
+        t[0].compare(t1[0])
+        t[0].compare(t1[0], variable=df[0].data.columns[0])
+        t[0].compare(t1[0], variable=[df[0].data.columns[0], df[0].data.columns[1]])
+
+        # there are also datasets, which don't contain given columns
+        t_2 = pdp.transform(variables=[df[0].data.columns[0], df[0].data.columns[1]])
+        t_2[0].plot(variable=df[0].data.columns[0])
+        t_2[0].plot(variable=df[0].data.columns[1])
+        assert len(t_2[1].results.items()) == 0
+
+        t[2][0].plot(variable=df[2][0].data.columns[0])
+
+        try:
+            t_2[0].plot(variable=df[0].data.columns[2])
+            assert False
+        except (Exception,):
+            pass
 
     except (Exception,):
         assert False
