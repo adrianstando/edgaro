2b92e7ce2bca642d93c5393256f55f7c6dc98ec0
adrianstando <adrsnek11@live.com>
2022-11-20 20:18:53 +0100

    Remove default legend in plot from pandas and refactor code

--- EDGAR/explain/PDPResult.py
+++ EDGAR/explain/PDPResult.py
@@ -35,6 +35,88 @@ class PDPResult:
         else:
             return None
 
+    def __plot_not_add(self, variable, ax, figsize, show_legend, y_lim):
+        curve = self.results[variable]
+        if curve is None:
+            raise Exception('Variable is not available!')
+
+        if ax is not None:
+            plt.sca(ax)
+        elif figsize is not None:
+            plt.subplots(figsize=figsize)
+
+        if variable not in self.categorical_columns:
+            plt.plot(curve.x, curve.y)
+        else:
+            plt.bar(curve.x, curve.y)
+
+        if self.curve_type == 'PDP':
+            plt.title("PDP curve for variable: " + variable)
+        elif self.curve_type == 'ALE':
+            plt.title("ALE curve for variable: " + variable)
+        else:
+            raise Exception('Wrong curve type!')
+
+        plt.xlabel(variable)
+        if show_legend:
+            plt.legend([self.name])
+        if y_lim is not None:
+            plt.ylim(y_lim)
+
+    def __plot_add_continuous(self, ax, figsize, curve_base, curves_add):
+        if ax is not None:
+            plt.sca(ax)
+        elif figsize is not None:
+            plt.subplots(figsize=figsize)
+
+        plt.plot(curve_base.x, curve_base.y)
+        for curve in curves_add:
+            plt.plot(curve.x, curve.y)
+
+    def __plot_add_categorical(self, variable, ax, figsize, curve_base, curves_add, add_plot_names):
+        df = pd.DataFrame({
+            'x': curves_add[0].x,
+            self.name: curve_base.y
+        })
+        for i in range(len(curves_add)):
+            df[add_plot_names[i]] = curves_add[i].y
+
+        if ax is not None:
+            df.plot(x='x', kind='bar', ax=ax, legend=False, xlabel=variable)
+        else:
+            df.plot(x='x', kind='bar', figsize=figsize, legend=False, xlabel=variable)
+
+        plt.xticks(rotation=0)
+
+    def __plot_add(self, variable, ax, figsize, curve_base, curves_add, add_plot, show_legend, y_lim):
+        curves_add = [c for c in curves_add if c is not None]
+        add_plot_names = [c.name for c in add_plot if c.results[variable] is not None]
+
+        p = 1
+        for ind in range(len(add_plot_names)):
+            if add_plot_names[ind] == self.name:
+                add_plot_names[ind] += ('_' + str(p))
+                p += 1
+
+        if variable not in self.categorical_columns:
+            self.__plot_add_continuous(ax, figsize, curve_base, curves_add)
+
+        else:
+            self.__plot_add_categorical(variable, ax, figsize, curve_base, curves_add, add_plot_names)
+
+        if self.curve_type == 'PDP':
+            plt.title("PDP curve for variable: " + variable)
+        elif self.curve_type == 'ALE':
+            plt.title("ALE curve for variable: " + variable)
+        else:
+            raise Exception('Wrong curve type!')
+
+        if show_legend:
+            plt.legend([self.name] + add_plot_names)
+        if y_lim is not None:
+            plt.ylim(y_lim)
+        plt.xlabel(variable)
+
     def plot(self, variable: str, figsize: Optional[Tuple[int, int]] = (8, 8),
              add_plot: Optional[List[PDPResult]] = None, ax: Optional[Axes] = None,
              show_legend: bool = True, y_lim: Optional[Tuple[float, float]] = None
@@ -43,32 +125,7 @@ class PDPResult:
             figsize = (8, 8)
 
         if add_plot is None:
-            curve = self.results[variable]
-            if curve is None:
-                raise Exception('Variable is not available!')
-
-            if ax is not None:
-                plt.sca(ax)
-            elif figsize is not None:
-                plt.subplots(figsize=figsize)
-
-            if variable not in self.categorical_columns:
-                plt.plot(curve.x, curve.y)
-            else:
-                plt.bar(curve.x, curve.y)
-
-            if self.curve_type == 'PDP':
-                plt.title("PDP curve for variable: " + variable)
-            elif self.curve_type == 'ALE':
-                plt.title("ALE curve for variable: " + variable)
-            else:
-                raise Exception('Wrong curve type!')
-
-            plt.xlabel(variable)
-            if show_legend:
-                plt.legend([self.name])
-            if y_lim is not None:
-                plt.ylim(y_lim)
+            self.__plot_not_add(variable, ax, figsize, show_legend, y_lim)
         else:
             curve_base = self.results[variable]
             if curve_base is None:
@@ -78,71 +135,11 @@ class PDPResult:
             if None in curves_add:
                 warnings.warn(f'There is not variable {variable} in one of the added plots!')
 
-            curves_add = [c for c in curves_add if c is not None]
-            add_plot_names = [c.name for c in add_plot if c.results[variable] is not None]
-
             if len(curves_add) == 0:
                 warnings.warn(f'None of the added plots have variable called {variable}!')
-
-                if ax is not None:
-                    plt.sca(ax)
-                elif figsize is not None:
-                    plt.subplots(figsize=figsize)
-
-                if self.curve_type == 'PDP':
-                    plt.title("PDP curve for variable: " + variable)
-                elif self.curve_type == 'ALE':
-                    plt.title("ALE curve for variable: " + variable)
-                else:
-                    raise Exception('Wrong curve type!')
-
-                if variable not in self.categorical_columns:
-                    plt.plot(curve_base.x, curve_base.y)
-                else:
-                    plt.bar(curve_base.x, curve_base.y)
-
-                if show_legend:
-                    plt.legend([self.name])
-                if y_lim is not None:
-                    plt.ylim(y_lim)
+                self.__plot_not_add(variable, ax, figsize, show_legend, y_lim)
             else:
-                if variable not in self.categorical_columns:
-
-                    if ax is not None:
-                        plt.sca(ax)
-                    elif figsize is not None:
-                        plt.subplots(figsize=figsize)
-
-                    plt.plot(curve_base.x, curve_base.y)
-                    for curve in curves_add:
-                        plt.plot(curve.x, curve.y)
-                else:
-                    df = pd.DataFrame({
-                        'x': curves_add[0].x,
-                        self.name: curve_base.y
-                    })
-                    for i in range(len(add_plot)):
-                        df[add_plot_names[i]] = curves_add[i].y
-
-                    if ax is not None:
-                        df.plot(x='x', kind='bar', ax=ax)
-                    else:
-                        df.plot(x='x', kind='bar', figsize=figsize)
-
-                    plt.xticks(rotation=0)
-
-                if self.curve_type == 'PDP':
-                    plt.title("PDP curve for variable: " + variable)
-                elif self.curve_type == 'ALE':
-                    plt.title("ALE curve for variable: " + variable)
-                else:
-                    raise Exception('Wrong curve type!')
-
-                if show_legend:
-                    plt.legend([self.name] + add_plot_names)
-                if y_lim is not None:
-                    plt.ylim(y_lim)
-            plt.xlabel(variable)
+                self.__plot_add(variable, ax, figsize, curve_base, curves_add, add_plot, show_legend, y_lim)
 
     def compare(self, other: PDPResult, variable: Optional[Union[str, List[str]]] = None):
         if isinstance(variable, str):
