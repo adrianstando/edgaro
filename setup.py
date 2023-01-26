aa9bd4126f9d2c99ca143ff0135761f4c1a58895
adrianstando <adrsnek11@live.com>
2023-01-26 21:19:41 +0100

    Fix workflow

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ setup(
     author="Adrian Stańdo",
     author_email="adrian.j.stando@gmail.com",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
